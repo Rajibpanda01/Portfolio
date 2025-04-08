cd531e5efc31891ecb4f71e6afce1876062f6b51
Rajib Panda <rajipanda09@gmail.com>
2025-04-08 23:05:52 +0530

    new file are added
    new file are added

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    new Typed(".text", {
+      strings: ["Frontend Developer", "Backend Developer", "Full Stack Developer"],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+    });
+  });
